8153752fc207b6848ee056066fcbdbed49236402
Jam Risser <jam@jamrizzi.com>
2017-08-27 12:52:15 -0500

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,12 @@
+NAME = 'foundation'
+
+AUTHOR = 'Jam Risser'
+
+VERSION = '0.1.0'
+
+COPYRIGHT = '2017'
+
+BANNER = '''
+dot v''' + VERSION + '''
+Copyright (c) ''' + COPYRIGHT + ' ' + AUTHOR + '''
+'''
